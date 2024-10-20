f1ee1ec733c8faadf7975071bd0454d91fb9aa58
baopham <pqbao27@gmail.com>
2024-10-20 21:49:52 +0700

    .

--- src/components/features/introduction/IntroductionPage.tsx
+++ src/components/features/introduction/IntroductionPage.tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image'
+
 export const IntroductionPage = () => {
   return (
     <div
       className={`min-h-screen bg-red-300 relative bg-[url('/images/introduction/bg.webp')] bg-cover bg-center`}
     >
-      1
+      <div className="fixed inset-0 bg-[#D9D9D9] opacity-70" />
+      <div className="w-[350px] h-[350px] rounded-[50%] bg-white">
+        <Image
+          alt="bg"
+          src={'/images/logo.png'}
+          width={350}
+          height={350}
+          className="object-cover"
+          priority
+        />
+      </div>
+
+      <div className="mt-5 w-[303px] h-[85px] px-[22px] gap-[30px] rounded-[20px]">
+        <p className="text-[30px]">LET’S GO</p>
+      </div>
     </div>
   )
 }
