ee69fe374d50c22f03e0c4f81b7dc589ea4ee3fc
baopham64uete <baopq@solashi.com>
2024-10-23 20:24:12 +0900

    introduction

--- src/components/features/introduction/IntroductionPage.tsx
+++ src/components/features/introduction/IntroductionPage.tsx
@@ -1,51 +1,43 @@
-import { cn } from '@/libs/utils'
-import MotorbikeSvg from '@public/svgs/motorbike.svg'
-import { Saira_Stencil_One } from 'next/font/google'
+import SkewButton from '@/components/shared/buttons/SkewButton'
+import { Container } from '@mantine/core'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const sairaStencilOne = Saira_Stencil_One({
-  subsets: ['latin'],
-  weight: ['400'],
-})
-
 export const IntroductionPage = () => {
   return (
-    <div className="min-h-screen relative bg-[url('/images/introduction/bg.webp')] bg-cover bg-center flex items-center justify-center">
+    <div className="min-h-screen relative bg-[url('/images/introduction/bg.jpeg')] bg-cover bg-center">
       <div className="absolute inset-0 bg-[#D9D9D9] bg-opacity-20 mix-blend-multiply" />
-      <div className="flex flex-col items-center z-10">
-        <div className="w-[350px] h-[350px] rounded-full bg-white overflow-hidden">
-          <Image
-            alt="Logo"
-            src="/images/logo.png"
-            width={350}
-            height={350}
-            className="object-cover"
-            priority
-          />
+
+      <Container size="xl" className="flex gap-5 py-5">
+        <div className="basis-1/3 flex flex-col z-10">
+          <div className="w-[250px] h-[250px] rounded-full bg-white overflow-hidden">
+            <Image
+              alt="Logo"
+              src="/images/logo.png"
+              width={250}
+              height={250}
+              className="object-cover"
+              priority
+            />
+          </div>
+
+          <div className="flex flex-col gap-4 mt-20">
+            <h1 className="text-4xl font-bold text-white">SAIGON URBAN TOUR</h1>
+            <p className="text-white">
+               a type specimen book. It has survived not only five centuries,
+              but also the leap electronic tktop publishing software like Aldus
+              PageMaker including versions of Lorem Ipsum but also the leap
+              electronic tktop publishing software like Aldus Page.
+            </p>
+
+            <Link href="/home" prefetch className="mt-10">
+              <SkewButton>Start Exploring</SkewButton>
+            </Link>
+          </div>
         </div>
-        <Link
-          href={'home'}
-          prefetch
-          className="mt-8 flex-responsive gap-2 md:gap-6 justify-center items-center rounded-[20px] bg-black p-4"
-        >
-          <Image
-            src={MotorbikeSvg.src}
-            width={74}
-            height={80}
-            alt="motorbike"
-            priority
-          />
-          <p
-            className={cn(
-              'text-[20px] md:text-[30px] text-center text-white font-bold',
-              sairaStencilOne.className,
-            )}
-          >
-            LET'S GO
-          </p>
-        </Link>
-      </div>
+
+        <div className="basis-2/3 flex flex-col z-10">2222</div>
+      </Container>
     </div>
   )
 }
