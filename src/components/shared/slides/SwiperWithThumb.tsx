708d268b686210c0fbf3e9a78e7def8524bd03ce
baopham64uete <baopq@solashi.com>
2024-12-30 16:20:07 +0900

    update UI

--- src/components/shared/slides/SwiperWithThumb.tsx
+++ src/components/shared/slides/SwiperWithThumb.tsx
@@ -126,6 +126,7 @@ export const SwiperWithThumb = ({
           <>
             <Button
               onClick={handlePrevClick}
+              className="hidden lg:block"
               style={{
                 position: 'absolute',
                 width: 48,
@@ -143,6 +144,7 @@ export const SwiperWithThumb = ({
             </Button>
             <Button
               onClick={handleNextClick}
+              className="hidden lg:block"
               style={{
                 position: 'absolute',
                 width: 48,
